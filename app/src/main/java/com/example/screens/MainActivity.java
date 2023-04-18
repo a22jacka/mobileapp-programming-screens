96704ad825d628575e4ed6b40eef994ff75dc028
a22jacka <a22jacka@student.his.se>
2023-04-18 09:34:58 +0200

    added a second activity and a button to (hopefully) go to it
    program froze so button not fully tested.
    updated som stuff but that's just how it's gotta be

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loadAct2(View view2) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        Log.i("MainActivity", "loading 2");
+        startActivity(intent);
     }
 }
