c9522521954da7bd2dfd1f26939ebe311b6390a8
a22jacka <a22jacka@student.his.se>
2023-04-20 16:07:12 +0200

    added functionality to input fields

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void loadAct2(View view2) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra("thing", "people");
-        intent.putExtra("numberOf", 2856);
-        startActivity(intent);
+
+        EditText things = findViewById(R.id.editTextThing);
+        EditText numberOf = findViewById(R.id.editTextNumber);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String thingsText = things.getText().toString();
+                int numberOfText = Integer.parseInt(numberOf.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("thing", thingsText);
+                intent.putExtra("numberOf", numberOfText);
+                startActivity(intent);
+            }
+        });
     }
 }
